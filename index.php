944c6a4b042e5176100b4939542fd2b22735f281
redespublitec <redes@publitecperu.com>
2018-09-30 16:09:53 -0500

    login
    Se realizo la vista del login, falta agregar la interacción con BBDD.
    
    continuar...

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
-<html>
+<html id="body">
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>LOGIN</title>
-	<link rel="stylesheet" type="text/css" href="css/login.css">
+	<link rel="stylesheet" type="text/css" href="css/index.css">
 </head>
-<body>
+<body >
 
 	<div id="block1">
 	</div>
-	<div id="block2">
-		<div id="espacio"></div>
-		<div id="blockform">
 
-			<div id="titulo"><em>LOGIN</em></div>
+	<div id="block2">
+		<div id="blockform">
+			<div class="titulo"><em>LOGIN</em></div>
 			<div id="cuerpo">
 				<label class="usuario">Usuario</label>
 				<input class ="user" type="text" name="user" autofocus="true">
@@ -22,8 +22,8 @@
 				<input class="submit" type="submit" name="enviar" value="INGRESAR" >
 				<a href="#">¿Olvido su Contraseña?</a>
 			</div>
-			
 		</div>
 	</div>
+
 </body>
 </html>
