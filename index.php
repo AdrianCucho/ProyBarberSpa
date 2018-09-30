e88771aaa4e10646685003129591506a1c0c81ab
AdrianCucho <adriancucho@gmail.com>
2018-09-30 01:48:23 -0500

    login1
    1

--- index.php
+++ index.php
@@ -2,8 +2,28 @@
 <html>
 <head>
 	<title>LOGIN</title>
+	<link rel="stylesheet" type="text/css" href="css/login.css">
 </head>
 <body>
 
+	<div id="block1">
+	</div>
+	<div id="block2">
+		<div id="espacio"></div>
+		<div id="blockform">
+
+			<div id="titulo"><em>LOGIN</em></div>
+			<div id="cuerpo">
+				<label class="usuario">Usuario</label>
+				<input class ="user" type="text" name="user" autofocus="true">
+				<label class="clave">Contraseña</label>
+				<input class ="password" type="password" name="password">
+
+				<input class="submit" type="submit" name="enviar" value="INGRESAR" >
+				<a href="#">¿Olvido su Contraseña?</a>
+			</div>
+			
+		</div>
+	</div>
 </body>
 </html>
